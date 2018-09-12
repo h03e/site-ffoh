de2b1e51b9169607dd6f8e09c366d31cf8c409d9
coco <coco@h03e.net>
2018-09-12 13:30:55 +0200

    Changes for tunneldigger-support + some restructuring

--- site-ffoh.mk
+++ site-ffoh.mk
@@ -1,33 +1,38 @@
+# Gluon site.mk for Freifunk-Ostholstein
+# Basic documentation at https://gluon.readthedocs.io/en/v2018.1.x/user/site.html
+
 GLUON_FEATURES := \
 	autoupdater \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
 	ebtables-limit-arp \
 	mesh-batman-adv-15 \
-	mesh-vpn-fastd \
 	radvd \
 	respondd \
 	status-page \
 	web-advanced \
-	web-wizard
+	web-wizard \
+	alfred \
+	mesh-vpn-tunneldigger
+	# mesh-vpn-fastd
 
 GLUON_SITE_PACKAGES := \
-	gluon-alfred \
-	respondd-module-airtime \
 	haveged \
-	gluon-status-page-mesh-batman-adv \
-	iwinfo
-	#gluon-mesh-vpn-tunneldigger \
+	iwinfo \
+	respondd-module-airtime
 
-DEFAULT_GLUON_RELEASE := 0.9.9~exp$(shell date '+%Y%m%d')
+# Always call `make` from the command line with the desired release version!
+# otherwise this is generated:
+DEFAULT_GLUON_RELEASE := 0.10.1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+GLUON_BRANCH ?= stable
 
 GLUON_LANGS ?= en de
-
 GLUON_REGION=eu
-GLUON_BRANCH=experimental
+
+# Enable images for ath10k devices with 802.11s mode
 GLUON_ATH10K_MESH=11s
